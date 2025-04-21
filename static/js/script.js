9365982d910ace69909c3a21468199851cdd9dd3
Gustavo Rivero <gusir22@gmail.com>
2025-04-21 13:59:19 -0400

    Created outputResults() to display validation results on #results-div

--- static/js/script.js
+++ static/js/script.js
@@ -3,9 +3,22 @@ function validatePhoneNumber(form) {
 
     if (!phoneNumber) {
         alert("Please provide a phone number");
+    } else {
+        outputResults(phoneNumber, true); // output validation results to #results-div
     }
 }
 
 function clearForm(form) {
     form.userInput.value = "";
+}
+
+function outputResults(phoneNumber, validFlag) {
+    let validState; // init empty valid state string for output message
+    if (validFlag) {
+        validState = "Valid"
+    } else {
+        validState = "Invalid"
+    }
+
+    document.getElementById('results-div').innerHTML = `${validState} US number: ${phoneNumber}`;
 }
